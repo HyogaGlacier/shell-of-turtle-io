53fd0230b59516b890fa3559d647f646e7ab2ac1
replem_w <Ok>
2017-09-10 11:26:18 +0900

    Shellの表示を追加

--- src/client/js/global.js
+++ src/client/js/global.js
@@ -4,12 +4,11 @@ module.exports = {
     KEY_ENTER: 13,
     KEY_CHAT: 13,
     KEY_FIREFOOD: 119,
-    KEY_SPLIT: 32,
+    KEY_SPACE: 32,
     KEY_LEFT: 37,
     KEY_UP: 38,
     KEY_RIGHT: 39,
     KEY_DOWN: 40,
-    KEY_SPACE: 121,
 
     borderDraw: false,
     spin: -Math.PI,
